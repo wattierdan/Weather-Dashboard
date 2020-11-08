be1584d721fbf90e99cf326dd350aaaf908d2d7f
Dan <wattierdan@live.com>
2020-11-07 21:00:50 -0800

    updates README, refactors code

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,3 @@
-/*
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-WHEN I open the weather dashboard
-THEN I am presented with the last searched city forecast
-*/
-
-//display date and weather icon next to name in current weather
-//uv index color coding
-//display date and weather icon in 5 day 
-
 var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="
 var oneCall = "https://api.openweathermap.org/data/2.5/onecall?"
 var key = "&appid=567045e16bb34f6e03d7d4dc194e6660"
@@ -30,14 +10,10 @@ var iconURL = "http://openweathermap.org/img/w/"
 var iconCode = ""
 var iconSpec = ".png"
 
-
 //get date
-
 var today = new Date();
-
 var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 console.log(date)
-
 
 //call to get lat & lng, print city name
 function weatherData() {
@@ -61,9 +37,7 @@ function weatherData() {
             $('.humidity').html("Humidity: " +  response.current.humidity + "%")
             $('.windSpeed').html("Wind Speed: " +  response.current.wind_speed + " MPH")
             $('.uvIndex').html("UV Index: " + response.current.uvi)
-
             //what color is the uvIndex?
-            
             if (response.current.uvi < 3) {
                 $('.uvIndex').css("border-color", "chartreuse")
             } else if (response.current.uvi < 6) {
@@ -75,12 +49,8 @@ function weatherData() {
             } else {
                 $('.uvIndex').css("border-color", "purple")
             }
-            
             iconCode = response.current.weather[0].icon
-            console.log(iconCode)
             $('#weatherIcon').attr('src', iconURL + iconCode + iconSpec)
-            
-
             //emptys five day forcast div
             $('.fiveDay').empty()
             //creates dayCards and prints weather data
@@ -92,28 +62,18 @@ function weatherData() {
                 var fiveDayIcon = iconURL + iconCode + iconSpec
                 var theDay = (today.getDate() + 1)
                 var nextDay = parseInt(theDay) + parseInt([i])
-                console.log(nextDay)
-
                 var newDate = (today.getMonth()+1)+'/'+ nextDay +'/'+today.getFullYear()
-
                 dayCard.html(
                     newDate +
                     '<img class="fiveDayIcon" src=' + '"' + fiveDayIcon + '"' + '>' +
                     "Temperature: " + Math.round(dayTemp) + "°F" + "<br>" + 
                     "Humidity: " + response.daily[i].humidity + "%"  
                 )
-            
-        
                 fiveDay.append(dayCard)
-            }
-            
+            } 
         })
     })
 }
-
-
-
-
 //saves to local storage 
 function savecity() {
     var str = JSON.stringify(savedCities)
@@ -163,11 +123,6 @@ window.onload = function() {
     weatherData()
     printSavedCites()
 }
-
-//if search button is clicked the lastCity = input val
-//if citybtn is clicked lastCity = citybtn this textcontent
-
-
 //when user enters a city...
 $('form').on('submit', function(e){
     e.preventDefault()
